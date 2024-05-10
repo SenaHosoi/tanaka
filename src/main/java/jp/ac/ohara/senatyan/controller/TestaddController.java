aba0a59e78a4097b06d0deeb262bbdbdfdd9d6a4
MaikoMori <fko2347019@stu.o-hara.ac.jp>
2024-05-10 10:19:38 +0900

    成績登録(データベース保存されていない)

--- src/main/java/jp/ac/ohara/senatyan/controller/TestaddController.java
+++ src/main/java/jp/ac/ohara/senatyan/controller/TestaddController.java
@@ -24,23 +24,21 @@ public class TestaddController {
 	private GakuseiService gakuseiService;
     
     @GetMapping("/testadd/")
-    public String index(Model model) {
+    public String index(Model model,GakuseiHyou gakuseihyou) {
         List<TestModel> tests = testaddService.getTestList();
         model.addAttribute("tests", tests);
+        model.addAttribute("data",gakuseihyou);
         System.out.println(tests);
         return "testadd";
     }
     @PostMapping("/testadd/")
     public String handleListActions(
-    		@RequestParam(name = "entYear", required = false) Integer entYear,
-    		@RequestParam(name = "classNum", required = false) String classNum,
-    		@RequestParam(name = "isAttend", required = false) Boolean isAttend,
+    		@RequestParam(name = "entYear") Integer entYear,
+    		@RequestParam(name = "classNum") String classNum,
     		Model model) {
     	// 検索操作の場合
-		
-        List<GakuseiHyou> students = gakuseiService.filterStudents(entYear, classNum, isAttend);
-        System.out.println("検索結果: " + students);
-        model.addAttribute("searchedStudents", students);
+        System.out.println(testaddService.filterStudents(entYear, classNum));
+        model.addAttribute("searchedStudents", testaddService.filterStudents(entYear, classNum));
         return "testadd"; // 検索結果のテンプレート名を返す
 	}
     
